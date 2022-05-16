7cf31c26b75883dad66fb1ad36828ac32f8651e7
CharlieGregg314 <99622838+CharlieGregg314@users.noreply.github.com>
2022-05-16 13:49:26 +1000

    Create struct.js

---
+++ struct.js
@@ -0,0 +1,51 @@
+const structures = {
+        "agree": {
+            "start": "{SEN}",
+            "structure": {
+                "{RSN}": [
+                    "it is",
+                    "I think that it's pretty cool that {SEN} But you have to believe that it's {TRU}, {ALS} {RSN}",
+                    "it's just {POS} {TRU}, {ALS} {RSN}",
+                    "it's {TRU}, and here's why; {SEN}.",
+                    "someone came up to me and said to me '{SEN}'",
+                    "I realised that; {SEN} And that's {POS} epic",
+                    "it's {POS} {POS} {TRU}"
+                ],
+                "{SEN}": [
+                    "It is just true.",
+                    "The truth is {POS} {TRU} because {RSN}. {SEN}",
+                    "It's {TRU} because {RSN}. {SEN}",
+                    "You have to believe it because {RSN}.",
+                    "I was walking down the road when suddenly I {POS} realised that {RSN}. {SEN}",
+                    "After discovering this I {POS} wanted to tell you so badly that {RSN}. {SEN}",
+                    "Someone asked me about this {POS} cool subject and I told them '{SEN}'. {SEN}",
+                    "{SEN} {ALS} - {SEN}"
+                ],
+                "{POS}": [
+                    "brilliantly",
+                    "radiantly",
+                    "tremendously",
+                    "undeniably",
+                    "positively",
+                    "amazingly",
+                    "inevitably {POS}",
+                    "tantilyzingly"
+                ],
+                "{TRU}": [
+                    "true",
+                    "correct",
+                    "accurate",
+                    "unerring",
+                    "factually {TRU}"
+                ],
+                "{ALS}": [
+                    "and",
+                    "also",
+                    "furthermore",
+                    "moreover",
+                    "in addition",
+                    "which {POS} implies"
+                ]
+            }
+        }
+    }
